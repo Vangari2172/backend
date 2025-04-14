55be38ff9b447e2a67c2f47ce416a350487e8690
Ramu Vangari <ramu.vangari@graymatrix.com>
2025-04-15 00:42:44 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@
 // });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
 
 
-connectDB();
+connectDB().then(()=> {
+    app.on("error", (error)=> {
+        console.log("Error connecting to database",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+}).catch((err)=> console.log("Error connecting to database",err))
 
 
 
---
+++ src/utils/apiError.js
@@ -0,0 +1,19 @@
+export class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong", errors = [],stack
+    ) {
+        super (message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.errors = errors;
+        this.stack = stack;
+        this.message = message;
+
+        if (stack){
+            this.stack = stack;
+        }else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
