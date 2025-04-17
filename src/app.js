eecf7abc1cae2612eef1fdf5388923477ca0a1fd
Ramu Vangari <ramu.vangari@graymatrix.com>
2025-04-18 00:02:44 +0530

    models and cloudinary

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import express from "express"
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended : true, limit : "16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-export {app};
+app.listen('8000', () => {
+    console.log("LISTENINBG ON PORT 8000")
+})
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //     path: "./env"
 // });
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+// import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
@@ -10,15 +10,15 @@ dotenv.config({
 });
 
 
-connectDB().then(()=> {
-    app.on("error", (error)=> {
-        console.log("Error connecting to database",error);
-        throw error;
-    })
-    app.listen(process.env.PORT || 8000, ()=> {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-}).catch((err)=> console.log("Error connecting to database",err))
+// connectDB().then(()=> {
+//     app.on("error", (error)=> {
+//         console.log("Error connecting to database",error);
+//         throw error;
+//     })
+//     app.listen(process.env.PORT || 8000, ()=> {
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//     })
+// }).catch((err)=> console.log("Error connecting to database",err))
 
 
 
