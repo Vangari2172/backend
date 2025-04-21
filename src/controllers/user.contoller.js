8291e510f2687553737ef216353ae0a77e73ab04
Ramu Vangari <ramu.vangari@graymatrix.com>
2025-04-21 19:20:34 +0530

    user controller in progress

--- src/controllers/user.contoller.js
+++ src/controllers/user.contoller.js
@@ -1,9 +1,58 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(500).json({
-        message: "User registered successfully Okkk"
-    })
-})
+  //TODO
+  //Get users details from request body
+  //validate user input
+  //check if user already exists
+  //check for images, check for avatar
+  //upload to cloudinar, avatar and cover image
+  //create user object - create entry in db
+  //remove password and remove refresh Token from response
+  //check for creation
+  //return response
 
-export { registerUser }
+  const { fullName, email, userName, password } = req.body;
+  console.log("first123", fullName, email, userName, password);
+
+  if (
+    ["fullName", "email", "userName", "password"]?.some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email }, { userName }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "Email or Username already exists");
+  }
+
+  const avatarLocalPath = req?.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req?.files?.coverImage?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Failed to upload avatar or cover image");
+  }
+
+  const user = User.create({
+    fullName,
+    email,
+    userName: userName?.toLowerCase(),
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
-//require("dotenv").config({
-//     path: "./env"
-// });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
+  path: "./env",
 });
 
-
-connectDB().then(()=> {
-    app.on("error", (error)=> {
-        console.log("Error connecting to database",error);
-        throw error;
-    })
-    app.listen(process.env.PORT || 8000, ()=> {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-}).catch((err)=> console.log("Error connecting to database",err))
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error connecting to database", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Error connecting to database", err));
 
 /*
 (async()=> {
